23641d409fb538e967e5d7d657dddf71eba0a334
河合 誠 <sin0306mk@gmail.com>
2014-11-15 11:03:58 +0900

    iosForSwift
    swift用プロジェクト

---
+++ iOSforSwift/iOSforSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSforSwift
+//
+//  Created by 河合 誠 on 2014/11/15.
+//  Copyright (c) 2014年 河合 誠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
