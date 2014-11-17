6b81bd7a197fc39583f20a247f797df8c3312c1a
SMB開発部 <sorimachi-aws@hotmail.com>
2014-11-17 17:09:12 +0900

    ラベルのプロパティ操作

--- iOSforSwift/iOSforSwift/ViewController.swift
+++ iOSforSwift/iOSforSwift/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         //FIXME:最初に画面が表示された時にする処理
         super.viewDidLoad()
         myLabel.text = "起動画面です"
+        myLabel.textAlignment = NSTextAlignment.Center
+        myLabel.textColor = UIColor.redColor()
+//        myLabel.backgroundColor = UIColor.cyanColor()
+        myLabel.font = UIFont.systemFontOfSize(14)
+        myLabel.numberOfLines = 0
+        
+        
         //TODO:色が白くなります
         //UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: false)
     }
